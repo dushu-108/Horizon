68d8144c09e128c630073d6e2b894773f825b886
Y. DUSHYANTH <yalamanchilidushyanth@gmail.com>
2026-01-24 15:08:02 +0530

    created auth routes

---
+++ backend/routes/auth.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends, HTTPException
+from schema.user import Token, UserCreate, UserLogin
+from auth import createAccessToken, verifyPassword, hashPassword
+from models.user import User
+from sqlalchemy.orm import Session
+from database import get_db
+
+router = APIRouter(
+    prefix = "/auth",
+    tags = ["Authentication"]
+)
+
+@router.post("/login", response_model=Token)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if not db_user or not verifyPassword(user.password, db_user.password):
+        raise HTTPException(status_code=401, detail="Incorrect email or password")
+    
+    access_token = createAccessToken(data={"sub": db_user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post("/register", response_model=User)
+def register(user : UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already exists")
+    
+    hashed_password = hashPassword(user.password)
+    new_user = User(email=user.email, name=user.name, avatar=user.avatar, password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+    
