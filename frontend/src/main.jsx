26bf3a53a01fc739d82b093466324571969c3e3d
Y. DUSHYANTH <yalamanchilidushyanth@gmail.com>
2026-01-30 00:10:40 +0530

    code polished and browser nesting removed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
@@ -11,9 +10,7 @@ import store from './redux/store.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </StrictMode>
 )
