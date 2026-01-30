fce32de136886316fe0430436d5d613e095d3a2b
Y. DUSHYANTH <yalamanchilidushyanth@gmail.com>
2026-01-30 15:30:20 +0530

    cors sorted

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from models import user, logo
 from routes import auth, googleAuth, logo
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
